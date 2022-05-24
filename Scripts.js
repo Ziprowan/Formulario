5ef08b402f7743eaff356a299516f0807328ca53
Ziprowan <jota_olaya@hotmail.com>
2022-05-23 19:53:50 -0500

    Progreso del proyecto

--- Scripts.js
+++ Scripts.js
@@ -1,19 +1,42 @@
-//listado de variables
+var FilaPersona = [];
+var conversor = [];
+var sName = "";
+var sApellido = "";
+var sDireccion = "";
 
-var Formulario =[];
-var arraycovertido =[];
-var Nombre ="";
-var Apellido ="";
-var Ubicacion ="";
 
-//guarda la informacion que llena el usuario en los 3 campos vacios
+//leer los datos 
+var LeerInformacion = function(){
+    conversor = JSON.parse(localStorage.getItem("Fila"))
+    FilaPersona = conversor
+    console.log(conversor)
+    document.getElementById("PersonasGuardadas").innerHTML = ""
 
-var GuardarInfo = function(){
-    var Nombre = document.getElementById("Nombres").value
-    var Apellido = document.getElementById("Apellidos").value
-    var Direccion = document.getElementById("Direccion").value
-    console.log(Nombre, Apellido, Direccion)
-    Formulario.push({Nombre,Apellido,Direccion})
-    localStorage.setItem("PersonaInfo", JSON.stringify(Formulario))
-    leerinformacion()
+    for (let a = 0; a < conversor.length; a++) {
+        document.getElementById("PersonasGuardadas").innerHTML += "<tr> <td>"+ conversor[a] +"</td><td>"+ conversor[a] +"</td><td>"+ conversor[a] +"</td> <td onclick="Eliminar("+ a +")">Delete</td></tr>"
+
+    }
+
 }
+
+//Guardar datos en consola y local storage
+var guardar = function(){
+    var sName = document.getElementById("txtName").value;
+    var sApellido = document.getElementById("txtApellido").value;
+    var sDireccion = document.getElementById("txtAdress").value;
+    console.log(sName,sApellido,sDireccion)
+
+    FilaPersona.push({sName,sApellido,sDireccion})
+    localStorage.setItem("Fila", JSON.stringify(FilaPersona))
+    LeerInformacion()
+}
+
+//Elimina fila de datos, guarda el cambio y lo lee
+var Eliminar = function(indice){
+    console.log(indice)
+    FilaPersona.splice(indice,1)
+    localStorage.setItem("Fila", JSON.stringify(FilaPersona))
+    LeerInformacion()
+}
+
+
